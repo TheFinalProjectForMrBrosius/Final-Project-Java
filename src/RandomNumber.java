71fe5caf00f886181850d5e60a15156138acdb3b
DakotaWolfe <54542632+DakotaWolfe@users.noreply.github.com>
2019-10-10 09:55:15 -0500

    commit

---
+++ src/RandomNumber.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class RandomNumber {
+
+    public static void main(String[] args){
+        Random rand = new Random();
+        int rand_pick = rand.nextInt();
+    }
+}
